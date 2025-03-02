1e7a979548dcfea3719d298b088e1d42addd7e21
finstory <facu995electro@hotmail.com>
2025-03-02 09:12:08 -0300

    prepare basic branch

--- src/application/auth/auth.module.ts
+++ src/application/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/_common/database/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
 export class AuthModule {
-  
+
 }
